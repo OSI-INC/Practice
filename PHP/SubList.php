9aae277b1325dcf7691bbb1c4021ad1d429c066f
Kevan Hashemi <hashemi@opensourceinstruments.com>
2024-12-19 16:04:28 -0500

    Change array index in PHP.

--- PHP/SubList.php
+++ PHP/SubList.php
@@ -10,14 +10,14 @@ li:hover {
 }
 </style>
 
-<ul class="sidebar">
+<ul>
 <?php
-	foreach ($subfiles as $sub) {
+	foreach ($subfiles as $value) {
 		print("\t<li");
-		if ($_SERVER['SCRIPT_NAME'] === '/' . $sub . '.php') {
+		if ($_SERVER['SCRIPT_NAME'] === '/' . $value . '.php') {
 			print(' class="selected"');
 		}
-		print('><a href="' . $sub . '.php">' . $sub . '</a></li>');
+		print('><a href="' . $value . '.php">' . $value . '</a></li>');
 		print("\n");
 	}
 ?>
